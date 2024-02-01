e1be965bc01608a978940de686757e940afd3488
LMarsol <lucasmarsolv@hotmail.com>
2024-02-01 00:32:18 -0300

    splash page and app binding

--- fe-lab-clinicas-self-service/lib/main.dart
+++ fe-lab-clinicas-self-service/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:fe_lab_clinicas_core/fe_lab_clinicas_core.dart';
+import 'package:fe_lab_clinicas_self_service/src/binding/lab_clinicas_application_binding.dart';
+import 'package:fe_lab_clinicas_self_service/src/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 
@@ -13,8 +15,9 @@ class LabClinicasSelfServiceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return LabClinicasCoreConfig(
       title: 'Lab Clínicas Auto Atendimento',
+      bindings: LabClinicasApplicationBinding(),
       builders: [
-        FlutterGetItPageBuilder(page: (_) => const SizedBox(), path: '/'),
+        FlutterGetItPageBuilder(page: (_) => const SplashPage(), path: '/'),
       ],
     );
   }
